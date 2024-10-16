80ab00d6c7dfa129e9cba5d673d5c6aaade93330
suqiangwei <suqiangwei@bytedance.com>
2024-10-16 14:27:25 +0800

    fix: 手动写cn-north-1

--- examples/Live/V20230101/DescribeIPAccessRuleExample.php
+++ examples/Live/V20230101/DescribeIPAccessRuleExample.php
@@ -3,7 +3,7 @@ include_once(__DIR__ . '/../../../vendor/autoload.php');
 
 use Byteplus\Service\Live\V20230101\Live;
 
-$client = Live::getInstance();
+$client = Live::getInstance("cn-north-1");
 
 // call below method if you dont set ak and sk in ~/.byteplus/config
 $client->setAccessKey("");
